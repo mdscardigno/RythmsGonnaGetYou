984884fbdd348eaed2595b9d2d5005a3256099f2
Milagro Scardigno <mdscardigno@gmail.com>
2022-12-27 10:58:27 -0500

    rest of the queries

--- testQueries.sql
+++ testQueries.sql
@@ -4,7 +4,7 @@ createdb RecordsDB
 --Access RecordsDB
 pgcli RecordsDB or UI
 
---Add a new band
+--Create Tables
 
 CREATE TABLE "Bands"(
 "Id" SERIAL PRIMARY KEY,
@@ -17,11 +17,6 @@ CREATE TABLE "Bands"(
 "ContactPhoneNumber" VARCHAR(16)
 );
 
---View all the bands
-
-Select * FROM "Bands"
-
---Add an album for a band
 CREATE TABLE "Albums"(
 "Id" SERIAL PRIMARY KEY,
 "Title" TEXT,
@@ -30,7 +25,6 @@ CREATE TABLE "Albums"(
 "BandId" INTEGER REFERENCES "Bands"
 );
 
---Add a song to an album
 CREATE TABLE "Songs"(
 "Id" SERIAL PRIMARY KEY,
 "Title" TEXT,
@@ -39,15 +33,31 @@ CREATE TABLE "Songs"(
 "AlbumId" INTEGER REFERENCES "Albums"
 );
 
+--View all the bands
+SELECT * FROM "Bands"
+
+--Add an album for a band
+INSERT INTO "Albums"("Title", "ReleaseDate", "isExplicit", "BandId")
+VALUES('The Black Parade', '2006-10-23', true, 1);
+
+--Add a song to an album
+INSERT INTO "Songs"("Title", "Duration", "TrackNumber", "AlbumId")
+VALUES('Welcome to the Black Parade', '00:03:45', 1, 1);
+
 --Let a band go (update isSigned to false)
+UPDATE "Bands" SET "IsSigned" = false WHERE "Id" = 1;
 
 --Resign a band (update isSigned to true)
-
+UPDATE "Bands" SET "IsSigned" = true WHERE "Id" = 1;
 
 --Given a band name, view all their albums
+SELECT * FROM "Albums" WHERE "BandId" = 1;
 
 --View all albums (and their associated songs) ordered by ReleaseDate
+SELECT * FROM "Albums" ORDER BY "ReleaseDate";
 
 --View all bands that are signed
+SELECT * FROM "Bands" WHERE "IsSigned" = true;
 
 --View all bands that are not signed
+SELECT * FROM "Bands" WHERE "IsSigned" = false;
