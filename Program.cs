f7762a02da7040cf3a26c020d92b3f3b1ca7d13b
Milagro Scardigno <mdscardigno@gmail.com>
2022-12-31 13:13:18 -0500

    Greeting

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.Linq;
+using Pastel;
 
 
 namespace RythmsGonnaGetYou
@@ -7,7 +10,26 @@ namespace RythmsGonnaGetYou
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to The Rythms Gonna Get You Management System!");
+            static void DisplayGreeting()
+            {
+                //https://www.ascii-art-generator.org/
+
+                Console.WriteLine($"{"############################################################################################################################################################".Pastel(Color.DarkGoldenrod)}");
+                Console.WriteLine($"{"############################################################################################################################################################".Pastel(Color.DarkGoldenrod)}");
+
+                Console.WriteLine($"{"######  #     # #     # ####### #     # #     #     #####      #####  ####### #     # #     #    #        #####  ####### #######    #     # ####### #     #".Pastel(Color.Violet)}");
+                Console.WriteLine($"{"#     # #     #  #   #     #    #     # ##   ##    #     #    #     # #     # ##    # ##    #   # #      #     # #          #        #   #  #     # #     #".Pastel(Color.Violet)}");
+                Console.WriteLine($"{"#     # #     #   # #      #    #     # # # # #    #          #       #     # # #   # # #   #  #   #     #       #          #         # #   #     # #     #".Pastel(Color.Violet)}");
+                Console.WriteLine($"{"######  #######    #       #    ####### #  #  #     #####     #  #### #     # #  #  # #  #  # #     #    #  #### #####      #          #    #     # #     #".Pastel(Color.Violet)}");
+                Console.WriteLine($"{"#   #   #     #    #       #    #     # #     #          #    #     # #     # #   # # #   # # #######    #     # #          #          #    #     # #     #".Pastel(Color.Violet)}");
+                Console.WriteLine($"{"#    #  #     #    #       #    #     # #     #    #     #    #     # #     # #    ## #    ## #     #    #     # #          #          #    #     # #     #".Pastel(Color.Violet)}");
+                Console.WriteLine($"{"#     # #     #    #       #    #     # #     #     #####      #####  ####### #     # #     # #     #     #####  #######    #          #    #######  ##### ".Pastel(Color.Violet)}");
+
+                Console.WriteLine($"{"############################################################################################################################################################".Pastel(Color.DarkGoldenrod)}");
+                Console.WriteLine($"{"############################################################################################################################################################".Pastel(Color.DarkGoldenrod)}");
+
+            };
+            DisplayGreeting();
         }
     }
 }
